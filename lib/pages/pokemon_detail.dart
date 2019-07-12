a7245d937a8439d79a6fd15ae324ec38d9dc859f
Simen Daehlin <simen.dehlin@gmail.com>
2019-07-12 18:29:53 +0100

    Added icon

--- lib/pages/pokemon_detail.dart
+++ lib/pages/pokemon_detail.dart
@@ -6,6 +6,107 @@ class PokemonDetails extends StatelessWidget {
 
   const PokemonDetails({this.pokemon});
 
+  bodyWidget(BuildContext context) => Stack(
+        children: <Widget>[
+          Positioned(
+            height: MediaQuery.of(context).size.height / 1.5,
+            width: MediaQuery.of(context).size.width - 20,
+            left: 10.0,
+            top: MediaQuery.of(context).size.height * 0.1,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    height: 70,
+                  ),
+                  Text(
+                    pokemon.name,
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Height: ${pokemon.height}"),
+                  Text("Weight: ${pokemon.weight}"),
+                  Text(
+                    "Types",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: pokemon.type
+                        .map((t) => FilterChip(
+                              backgroundColor: Colors.amber,
+                              label: Text(t),
+                              onSelected: (b) {},
+                            ))
+                        .toList(),
+                  ),
+                  Text(
+                    "Weakness",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: pokemon.weaknesses
+                        .map((t) => FilterChip(
+                              backgroundColor: Colors.red,
+                              label: Text(
+                                t,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              onSelected: (b) {},
+                            ))
+                        .toList(),
+                  ),
+                  pokemon.nextEvolution == null
+                      ? Text("No Evolution")
+                      : Text(
+                          "Next Evolution",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                  pokemon.nextEvolution == null
+                      ? Row()
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: pokemon.nextEvolution
+                              .map((n) => FilterChip(
+                                    backgroundColor: Colors.green,
+                                    label: Text(
+                                      n.name,
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    onSelected: (b) {},
+                                  ))
+                              .toList(),
+                        ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Hero(
+              tag: pokemon.img,
+              child: Container(
+                height: 200.0,
+                width: 200.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(pokemon.img),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,50 +115,8 @@ class PokemonDetails extends StatelessWidget {
         backgroundColor: Colors.cyan,
         title: Text(pokemon.name),
       ),
-      
-      body: Stack(
-        children: <Widget>[
-          Container(
-            child: Column(
-              children: <Widget>[
-                Text(pokemon.name),
-                Text("Height: ${pokemon.height}"),
-                Text("Weight: ${pokemon.weight}"),
-                Text("Types"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: pokemon.type
-                      .map((t) => FilterChip(
-                            label: Text(t),
-                            onSelected: (b) {},
-                          ))
-                      .toList(),
-                ),
-                Text("Weakness"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: pokemon.weaknesses
-                      .map((t) => FilterChip(
-                            label: Text(t),
-                            onSelected: (b) {},
-                          ))
-                      .toList(),
-                ),
-                Text("Next Evolution"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: pokemon.nextEvolution
-                      .map((n) => FilterChip(
-                            label: Text(n.name),
-                            onSelected: (b) {},
-                          ))
-                      .toList(),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+      backgroundColor: Colors.cyan,
+      body: bodyWidget(context),
     );
   }
 }
