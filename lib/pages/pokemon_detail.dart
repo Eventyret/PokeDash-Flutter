37607a65d3ac8463dbabba5aea5e52e1be4a1aa6
Simen Daehlin <simen.dehlin@gmail.com>
2019-07-12 17:06:04 +0100

    Added starting container

--- lib/pages/pokemon_detail.dart
+++ lib/pages/pokemon_detail.dart
@@ -6,14 +6,18 @@ class PokemonDetails extends StatelessWidget {
 
   const PokemonDetails({this.pokemon});
 
+  bodyWidget() => Container();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         backgroundColor: Colors.cyan,
         title: Text(pokemon.name),
       ),
       backgroundColor: Colors.cyan,
+      body: bodyWidget(),
     );
   }
 }
